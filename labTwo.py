c054b9af86b49c4f2469c5b139b9a60f50ea1794
Bárbara Menezes <bmenezes@ciandt.com>
2021-03-30 18:12:39 -0300

    update functions

--- labTwo.py
+++ labTwo.py
@@ -17,12 +17,12 @@ def executar_query_github(query):
 
 headers = {
     'Content-Type': 'application/json',
-    'Authorization': 'Bearer 37c8b0b4a04cb51f9180f5cf58ca5d45013dcb4d'
+    'Authorization': 'Bearer d18243107692cb1101066f06ec48a628e7776dd3'
 }
 
 query = """
 query LabTwo {
-  search(query: "language:java and stars:>100 and is:public", type: REPOSITORY, first: 10 {endCursorCode}) {
+  search(query: "language:java,stars:>100,is:public", type: REPOSITORY, first: 10 {endCursorCode}) {
     pageInfo {
       hasNextPage
       endCursor
@@ -54,14 +54,14 @@ def formatar_datas(response):
 def get_repos():
   todos_resultados = []
   end_cursor = ''
-  for i in range(1):
+  for i in range(100):
     cursor, resultados = formata_query(end_cursor)
     end_cursor = cursor
     todos_resultados += resultados
   return todos_resultados
 
 def formata_query(end_cursor):
-  queryVar = query.replace("{endCursorCode}", "") if end_cursor == "" else query.replace("{endCursorCode}", ', after: "%s"' % end_cursor)
+  queryVar = query.replace("{endCursorCode}", ', after: "Y3Vyc29yOjQ5MA=="') if end_cursor == "" else query.replace("{endCursorCode}", ', after: "%s"' % end_cursor)
   response = executar_query_github(queryVar)
   formatar_datas(response)
   return response["data"]["search"]["pageInfo"]["endCursor"], response["data"]["search"]["nodes"]
--- main.py
+++ main.py
@@ -5,13 +5,19 @@ import calculaMetrics
 import pandas as pd
 
 def main():
+    rep = 0
     repositories = labTwo.get_repos()
     z = []
     for repo in repositories:
-        path = cloneGit.git_clone(repo)
-        metrics = calculaMetrics.calculaMetrica(path)
-        z.append(repo | metrics)  
-        deleteGit.clean_all_repositories(path)
+        try:
+            path = cloneGit.git_clone(repo)
+            rep = rep + 1
+            print(rep)
+            metrics = calculaMetrics.calculaMetrica(path)
+            z.append(repo | metrics)  
+            deleteGit.clean_all_repositories(path)
+        except Exception as e:
+            print(e)
     arquivo = pd.DataFrame(z)
     arquivo.to_csv('resultado.csv', index=False)
 
